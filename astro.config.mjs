ecbd96aa4dfb849c29aec2877fe78700ec4aa9fe
Nicolas Rame <nicorame3@gmail.com>
2026-01-24 11:36:50 -0300

    deplo 5

--- astro.config.mjs
+++ astro.config.mjs
@@ -3,5 +3,7 @@ import { defineConfig } from 'astro/config';
 export default defineConfig({
   site: 'https://nicorame.github.io',
   base: '/f1nextrace',
-  trailingSlash: 'ignore'
+  build: {
+    format: 'file'
+  }
 });
